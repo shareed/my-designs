f0755682952519a8915e0daf32e238584adcffe2
shareed <shannontlreed@gmail.com>
2024-01-19 23:17:07 -0800

    removed code from desgins

--- src/components/menu/MenuMobile.js
+++ src/components/menu/MenuMobile.js
@@ -7,18 +7,7 @@ import Options from "./Options"
 const MenuMobile = ({matches}) => {
   return (
     <MenuContainer>
-    <IconButton>
-      <MenuIcon />
-    </IconButton>
 
-    <MenuHeader textAlign={"center"} variant="h4">
-      My Designs
-    </MenuHeader>
-
-    <IconButton>
-      <SearchIcon />
-    </IconButton>
-    <Options matches={matches} />
     </MenuContainer>
   )
 }

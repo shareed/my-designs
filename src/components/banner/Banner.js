32a899f2c6fd2e81e553ff8b5c8ecacb913bbcd5
shareed <shannontlreed@gmail.com>
2024-01-19 21:22:52 -0800

    changing layouts

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -7,16 +7,8 @@ const Banner = () => {
    const matches = useMediaQuery(theme.breakpoints.down('md')); 
  return (
    <BannerContainer>
-     <BannerImage src='/images/banner/womanOnComputer.png'/>
-     <BannerContent>  
-       <Typography variant='h6'>Huge Collection</Typography>
-       <BannerTitle variant='h2'>New Designs</BannerTitle>
-       <BannerDescription variant='subtitle'>
-         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-         eiusmod tempor incididunt ut labore et dolore magna aliqua.
-       </BannerDescription>
-     </BannerContent>
-   </BannerContainer>
+     
+      </BannerContainer>
  )
 }
 
